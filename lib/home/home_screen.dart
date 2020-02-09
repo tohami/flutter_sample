7cabf31403040b32676dd2134d506fdd420af2b0
tohami.hussin <tohami.hussin@linkdev.com>
2020-02-09 11:57:07 +0200

    add images assets

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -37,98 +37,106 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("News List"),
       ),
-      body: Stack(
-        children: <Widget>[
-          //result
-          _status.status == Status.SUCCESS
-              ? ListView.builder(
-                  itemCount: _status.data.length,
-                  itemBuilder: (context, index) {
-                    var item = _status.data[index];
-                    return Card(
-                      elevation: 6,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      margin: EdgeInsets.only(
-                          top: 10, right: 20, left: 20, bottom: 10),
-                      child: Container(
-                        height: 120,
-                        padding: EdgeInsets.only(left: 10, right: 10),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          children: <Widget>[
-                            //news image
-                            Container(
-                              height: 80,
-                              width: 80,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border:
-                                      Border.all(color: Colors.red, width: 1),
-                                  image: DecorationImage(
-                                      image: NetworkImage(item.imageUrl))),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-
-                            //title and info
-                            Expanded(
-                              child: Container(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/news_list_bg.png"),
+                fit: BoxFit.cover)),
+        child: Stack(
+          children: <Widget>[
+            //result
+            _status.status == Status.SUCCESS
+                ? ListView.builder(
+                    itemCount: _status.data.length,
+                    itemBuilder: (context, index) {
+                      var item = _status.data[index];
+                      return Card(
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        margin: EdgeInsets.only(
+                            top: 10, right: 20, left: 20, bottom: 10),
+                        child: Container(
+                          height: 120,
+                          padding: EdgeInsets.only(left: 10, right: 10),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              //news image
+                              Container(
                                 height: 80,
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: <Widget>[
-                                    //title
-                                    Expanded(
-                                      child: Container(
-                                        color: Colors.red,
-                                        padding: EdgeInsets.only(right: 40),
-                                        child: Text(
-                                          item.newsTitle,
-                                          maxLines: 3,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: TextStyle(fontWeight: FontWeight.bold),
+                                width: 80,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border:
+                                        Border.all(color: Colors.red, width: 1),
+                                    image: DecorationImage(
+                                        image: NetworkImage(item.imageUrl))),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+
+                              //title and info
+                              Expanded(
+                                child: Container(
+                                  height: 80,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      //title
+                                      Expanded(
+                                        child: Container(
+                                          color: Colors.red,
+                                          padding: EdgeInsets.only(right: 40),
+                                          child: Text(
+                                            item.newsTitle,
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
                                         ),
                                       ),
-                                    ) ,
 
-                                    //info
-                                    Row(
-                                      mainAxisSize: MainAxisSize.max,
-                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      children: <Widget>[
-                                        Text("Likes(${item.likes})") ,
-                                        Text("${item.numOfViews} Views") ,
-                                      ],
-                                    )
-                                  ],
+                                      //info
+                                      Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: <Widget>[
+                                          Text("Likes(${item.likes})"),
+                                          Text("${item.numOfViews} Views"),
+                                        ],
+                                      )
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  })
-              : Container(),
+                      );
+                    })
+                : Container(),
 
-          //error
-          _status.status == Status.ERROR
-              ? Center(
-                  child: Text(_status.error),
-                )
-              : Container(),
+            //error
+            _status.status == Status.ERROR
+                ? Center(
+                    child: Text(_status.error),
+                  )
+                : Container(),
 
-          //loading
-          _status.status == Status.LOADING
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Container()
-        ],
+            //loading
+            _status.status == Status.LOADING
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Container()
+          ],
+        ),
       ),
     );
   }
